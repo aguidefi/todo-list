a48f56f497e5c8731c39698c96ed79ebfbffc2d7
vickyaguidf <victoria.aguidefi@gmail.com>
2024-02-14 15:29:09 -0300

    creando to do list

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+let tareas = [
+  {
+    titulo:'tomar agua al despertar'
+    estado: false
+    id: 1
+  },
+  {
+    titulo: 'hacer ejercicio'
+    estado: false
+    id: 2
+  },
+  {
+    titulo: 'Escrituras matutinas'
+    estado: false
+    id: 3
+  }
+];
